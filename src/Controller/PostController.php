fb33a62c484deacefb33c1a5dd31e6639e8a8bd1
Pierre Bretéché <pbreteche@dawan.fr>
2022-05-17 13:57:22 +0200

    Doctrine repository findBy

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,10 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
+    private const MAX_POSTS_PER_PAGE = 20;
+
     #[Route('/post')]
     public function index(PostRepository $postRepository): Response
     {
-        $posts = $postRepository->findAll();
+        $posts = $postRepository->findBy([], ['createdAt' => 'DESC'], self::MAX_POSTS_PER_PAGE);
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
